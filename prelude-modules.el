b0253e10a803fcc5b526cc42aadbce886cc19958
xiaolangde <xiaolangde@gmail.com>
2014-10-18 18:33:04 +0800

    update prelude to Oct 17, a9f62cf727a0d7080bcc3d1

--- prelude-modules.el
+++ prelude-modules.el
@@ -4,6 +4,7 @@
 (require 'prelude-erc)
 (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
 (require 'prelude-helm) ;; Interface for narrowing and search
+;(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
@@ -17,10 +18,12 @@
 ;; (require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
+;; (require 'prelude-go)
 ;; (require 'prelude-haskell)
 (require 'prelude-js)
 ;; (require 'prelude-latex)
 (require 'prelude-lisp)
+;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 ;; (require 'prelude-python)
@@ -31,3 +34,4 @@
 ;; (require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
+;; (require 'prelude-yaml)
